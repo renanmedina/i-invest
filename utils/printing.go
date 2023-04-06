450ab37e16c1b2d9eb76f2614ed740ca2a72c19e
Renan Medina <renan@silvamedina.com.br>
2023-04-05 23:33:46 -0300

    Adding go-pretty package to print table styled

--- utils/printing.go
+++ utils/printing.go
@@ -3,21 +3,27 @@ package utils
 import (
 	"fmt"
 	"investment-warlock/investor"
+	"os"
+
+	"github.com/jedib0t/go-pretty/v6/table"
 )
 
 func PrintConsolidation(wallet investor.Wallet) {
-	fmt.Println("========================================================================")
+	fmt.Println("===========================================================")
 	fmt.Println("Wallet: ", wallet.Name)
 	fmt.Println("Cliente: ", wallet.Client.Name)
 	fmt.Println("Total investido: R$ ", wallet.Total())
-	fmt.Println("------------------------------------------------------------------------")
-	fmt.Println("| Ativo | \t Quantidade | \t Preço Médio | \t Total |")
+	writer := table.NewWriter()
+	writer.SetOutputMirror(os.Stdout)
+	writer.AppendHeader(table.Row{"Ativo", "Quantidade", "Preço Médio", "Total"})
 	for _, consolidatedAsset := range wallet.Consolidation {
 		asset := consolidatedAsset.Asset
 		quantity := consolidatedAsset.TotalQuantity
 		averagePrice := consolidatedAsset.AveragePrice
 		total := consolidatedAsset.TotalCost
-		fmt.Println(asset.Ticker, " | \t", quantity, " | \t", averagePrice, " | \t", total, " |")
+
+		writer.AppendRow([]interface{}{asset.Ticker, quantity, averagePrice, total})
 	}
-	fmt.Println("========================================================================")
+
+	writer.Render()
 }
